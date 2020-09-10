643154276e47dbc676fbca8bf24b6b2c4e36f7be
2407858590@qq.com <2407858590@qq>
2020-09-11 00:42:45 +0800

    Update index.css

--- T192-01-看漫画/张韩/navigationBar/navigationBar.js
+++ T192-01-看漫画/张韩/navigationBar/navigationBar.js
@@ -3,39 +3,34 @@
 //c:代表排行页地址
 //d:代表分类页地址
 //e:代表资讯页地址
-let path='navigationBar'
-
 function navigationBar(){
-	var inputImg="navigationBar/img/QQ截图20200908215548.png";
-	var logo="navigationBar/img/logo-kmh.png";
-	var userImg="navigationBar/img/visitor.png";
 	return new Vue({
 		el:'#navigationBar',
 		components:{
 			'navigationBar':{
-					template:`<div id="navigations">
-					<div id="navigationBarImg">
-						<img src="`+logo+`" />
+					template:`	<div id="navigations" style="z-index: 999;">
+				<div id="navigationBarImg">
+					<img src="./navigationBar/img/logo-kmh.png" />
+				</div>
+				<div id="navigationBarText">
+					<span><a href='#'>首页</a></span>
+					<span><a href='#'>更新</a></span>
+					<span><a href='#'>排行</a></span>
+					<span><a href='#'>分类</a></span>
+					<span><a href='#'>资讯</a></span>
+				</div>
+				<div id="navigationBarInput">
+					<input  type="text"/><button><img src="./navigationBar/img/QQ截图20200908215548.png"/></button>
+				</div>
+				<div id="navigationBarUser">
+					<img id="navigationBarUser-userImg" src="./navigationBar/img/visitor.png" >
+					<div id="navigationBarUser-text">
+						<span><a href="#">书架</a></span>
+						<span><a href="#">历史</a></span>
+						<span id="navigationBarUser-text-span"><a href=""><span ></span>下载app</a></span>
 					</div>
-					<div id="navigationBarText">
-						<span><a href=''>首页</a></span>
-						<span><a href=''>更新</a></span>
-						<span><a href=''>排行</a></span>
-						<span><a href=''>分类</a></span>
-						<span><a href=''>资讯</a></span>
-					</div>
-					<div id="navigationBarInput">
-						<input  type="text"/><button><img src="`+inputImg+`"/></button>
-					</div>
-					<div id="navigationBarUser">
-						<img id="navigationBarUser-userImg" src="`+userImg+`" >
-						<div id="navigationBarUser-text">
-							<span><a>书架</a></span>
-							<span><a>历史</a></span>
-							<span><a>下载app</a></span>
-						</div>
-					</div>
-				</div>`			
+				</div>
+			</div>`			
 			}	
 		}
 	})
