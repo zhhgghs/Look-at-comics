bab282e2fc9c5bcba9c4f6d0ea2513f3167bb715
2407858590@qq.com <2407858590@qq>
2020-09-27 19:24:02 +0800

    Update index.css
    更新

--- T192-01-看漫画/张韩/navigationBar/navigationBar.js
+++ T192-01-看漫画/张韩/navigationBar/navigationBar.js
@@ -6,31 +6,65 @@
 function navigationBar(){
 	return new Vue({
 		el:'#navigationBar',
+		data:{
+			popupOne:false,
+			popupTwoer:false,
+			popupSiriy:false,
+			user:{
+				name:'游客',
+				diamonds:0,
+				goldsCoin:0,
+				monthly :0,
+				recommend:0,
+				imgSrc:"./navigationBar/img/visitor.png",
+				sign :false,
+				books:null,
+				look:null
+			}
+		},
+		methods:{//方法
+			hover(isShow,number){
+				if(number==0){
+					this.popupOne=isShow;
+				}else if(number==1){
+					this.popupTwoer=isShow
+				}else{
+					this.popupSiriy=isShow
+				}
+			}
+		},
+		computed:{//计算属性
+			isBooks(){
+				return this.user.books==null;
+			},
+			isLook(){
+				return this.user.look==null;
+			},
+			popupOneStype(){
+				if(this.popupOne){
+					return "display: inline-block;right: 140px;"
+				}else{
+					return 'display: none;'
+				}
+			},
+			popupTwoerStype(){
+				if(this.popupTwoer){
+					return "display: inline-block;	right: 115px;"
+				}else{
+					return 'display: none;'
+				}
+			},
+			popupSiriyStype(){
+				if(this.popupSiriy){
+					return "display: inline-block;	right: 80px;"
+				}else{
+					return 'display: none;'
+				}
+			}
+		},
 		components:{
 			'navigationBar':{
-					template:`	<div id="navigations" style="z-index: 999;">
-				<div id="navigationBarImg">
-					<img src="./navigationBar/img/logo-kmh.png" />
-				</div>
-				<div id="navigationBarText">
-					<span><a href='#'>首页</a></span>
-					<span><a href='#'>更新</a></span>
-					<span><a href='#'>排行</a></span>
-					<span><a href='#'>分类</a></span>
-					<span><a href='#'>资讯</a></span>
-				</div>
-				<div id="navigationBarInput">
-					<input  type="text"/><button><img src="./navigationBar/img/QQ截图20200908215548.png"/></button>
-				</div>
-				<div id="navigationBarUser">
-					<img id="navigationBarUser-userImg" src="./navigationBar/img/visitor.png" >
-					<div id="navigationBarUser-text">
-						<span><a href="#">书架</a></span>
-						<span><a href="#">历史</a></span>
-						<span id="navigationBarUser-text-span"><a href=""><span ></span>下载app</a></span>
-					</div>
-				</div>
-			</div>`			
+					template:`	`			
 			}	
 		}
 	})
